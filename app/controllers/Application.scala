f9fe2d38232c252122d059662688db7daf069842
langelvs <langel.vicente@gmail.com>
2012-12-31 02:44:31 +0100

    Same index page using JADE

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,14 @@ import plugins.Scalate.engine
 object Application extends Controller {
   
   def index = Action {
+    Ok("Prueba de templates").as("text/html")
+  }
+  
+  def jade = Action {
+    Ok(engine.layout("/views/jade/index.jade")).as("text/html")
+  }
+  
+  def scaml = Action {
     Ok(engine.layout("/views/scaml/index.scaml")).as("text/html")
   }
   
