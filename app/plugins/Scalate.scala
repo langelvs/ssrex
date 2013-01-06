acd1aedbbff4cb204e943b6a926d0dd53692b697
langelvs <langel.vicente@gmail.com>
2013-01-06 22:40:56 +0000

    Generic writer and content-type implicits for Node and Element types.

--- app/plugins/Scalate.scala
+++ app/plugins/Scalate.scala
@@ -1,24 +1,24 @@
 package plugins
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Node
+import org.jsoup.nodes.Element
 import play.api.mvc.Codec
 import play.api.http.{ ContentTypeOf, Writeable }
 import play.api.Play.current
 
 object scalate {
 
-  implicit def contentTypeOfNode(implicit codec: Codec): ContentTypeOf[Node] = {
-    ContentTypeOf[Node](Some("text/html"))
+  implicit def contentTypeOfElement(implicit codec: Codec): ContentTypeOf[Element] = {
+    ContentTypeOf[Element](Some("text/html"))
   }
 
-  implicit def writableNode[T <: Node](implicit codec: Codec): Writeable[T] = Writeable[T]((e : T) => codec.encode(e.outerHtml))
+  implicit def writableElement[T <: Element](implicit codec: Codec): Writeable[T] = Writeable[T]((e : T) => codec.encode(e.html))
 
   def template(name: String) =
     Jsoup.parse(engine.layout(name))
 
   def fragment(name: String) =
-    Jsoup.parseBodyFragment(engine.layout(name)).body.html
+    Jsoup.parseBodyFragment(engine.layout(name)).body
   
   private[this] val engine =
     current.plugin[ScalatePlugin].head.engine
